4e1ee92d52eb7992134feb6ae17f67df87a67147
Cian <cian.odonnell@ucdconnect.ie>
2020-07-02 15:32:06 +0100

    Testing begins on HM4040

--- zookeeper/zookeeper.py
+++ zookeeper/zookeeper.py
@@ -1,15 +1,40 @@
+"""
+ZooKeeper
+~~~~~~~~~
+
+This is the entry point for the program.
+
+I've just used a script (for now)
+
+SCPI
+~~~~~~~~
+The SCPI package allows for easy interfacing
+with a VISA device
+
+"""
 import SCPI
 import pyvisa as visa
 import logging
 import sys
 
+# contains the device IDs
+DEVICES = ()
+backend = '@py'
 # setup logger
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG) 
 
-backend = '@sim'
-
-rm = visa.ResourceManager(backend)
-
 # find port from resource manager
-inst = SCPI.Instrument(port='ASRL2::INSTR', backend=backend)
-inst.connect()
+PSU = SCPI.Instrument(port='ASRL/dev/ttyUSB0::INSTR', backend=backend)
+PSU.connect()
+print(PSU.STB())
+print(PSU.id)
+input()
+PSU.disconnect()
+
+def setup_instruments(resource_manager=None):
+    
+    if resource_manager is None:
+        logging.critical("No resource manager specified")
+        raise ValueError
+
+    
