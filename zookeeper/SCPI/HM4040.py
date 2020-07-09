69e206035b84c6de0f5fa5b809afdb173c8b94f1
Cian O'Donnell <cian.odonnell@ucdconnect.ie>
2020-07-09 09:23:26 -0400

    Added some setter/getter functionality in HM4040 - untested

--- zookeeper/SCPI/HM4040.py
+++ zookeeper/SCPI/HM4040.py
@@ -1,8 +1,10 @@
 from .SCPI_Instrument import Instrument
 import logging
 
+ID = 'HAMEG,HMP4040,026043549,HW50020001/SW2.50\n'
+NUM_CHANNELS = 4
+VOLTAGE = {'MIN': 0.0, 'MAX' : 32.05}
 config = {
-    'id' : 'HAMEG,HMP4040,026043549,HW50020001/SW2.50\n'
     
     }
 
@@ -12,22 +14,56 @@ class HM4040(Instrument):
     def __init__(self, port=None, backend=''):
         # resource_params defined in config_dict
         super().__init__(port=port, backend=backend, resource_params=config)
-        
-    def connect(self):
-        # need exception handling here
-        super().connect()
-        if self.connected():
-            self.startup()
-        
-    def startup(self):
+        # self._startup()
+        # TODO! Should internally check if device is HM4040
+        logging.info("HM4040: Successfully instanciated")
+    
+    # TODO! implement repr 
+    def __repr__(self):
+        pass
+    
+    def _startup(self):
         self.CLS()
         self.RST()    
-    
-    def select_channel(channel=None):
-        if channel is None:
-            logging.warning("HM4040: No channel selected")
             
+    @property
+    def channel(self):
+        """
+        Queries the current device channel
+        """
+        return self.INST.NSEL()
+    
+    @channel.setter
+    def channel(self, channel : int = None):
+        """
+        Selects a channel using a numerical value
+        Number of channels physically defined by NUM_CHANNELS
         
-    #def change_channel(self, num=None):
+        channel: channel number (between and including 1 - 4)
         
-    #    self.channel.select(num)
+        """
+        if channel not in range(1, NUM_CHANNELS+1):
+            logging.warning("HM4040: Specified channel not available")
+            return
+        return self.INST.NSEL(channel)
+    
+    @property
+    def voltage(self, channel : int = None):
+        pass
+    
+    # setting voltages
+    @voltage.setter
+    def voltage(self, channel : int = None):
+        """
+        Sets the voltage on a specified channel to a value
+        between 0 [MIN] and 32.05 [MAX]
+        
+        channel :   optional channel value. if unspecified, defaults to
+                    current channel
+        """
+        if channel not in range(1, NUM_CHANNELS+1):
+            logging.warning("HM4040: Specified channel not available")
+            return
+        
+        # TODO! Implement voltage setter
+        pass
---
+++ zookeeper/Workbench.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  9 08:23:33 2020
+
+@author: cian
+"""
+
+# look at implementing a mutable mapping
+class Workbench(list):
+    pass
