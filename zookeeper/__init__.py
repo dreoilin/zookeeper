2c200057bc8710c095b5144e68abba2d8e78c714
Cian <cian.odonnell@ucdconnect.ie>
2020-07-30 11:08:26 +0100

    Changes to __init__.py

--- zookeeper/__init__.py
+++ zookeeper/__init__.py
@@ -1,4 +1,8 @@
 from .zookeeper import main
+from .Workbench import Workbench
+from .drivers.HMP4040 import HMP4040
+from .drivers.KS33522B import KS33522B
+from .drivers.SCPI.VISA_Instrument import VISA_Instrument
 from .__version__ import __version__
 
-__all__ = ['main']
+__all__ = ['main', 'Workbench', 'HMP4040', 'KS33522B', 'VISA_Instrument']
